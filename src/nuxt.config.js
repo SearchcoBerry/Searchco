1f816dbefcc325f57e68ecdb7633c0ab3ad0dacf
kome <yoneda886@gmail.com>
2021-02-26 20:42:46 +0900

    無限ホットリロードを修正

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -36,5 +36,16 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    /*
+    ** You can extend webpack config here
+    */
+    filenames: {
+      app: ({ isDev }) => isDev ? '[name].[hash].js' : '[chunkhash].js',
+      chunk: ({ isDev }) => isDev ? '[name].[hash].js' : '[chunkhash].js'
+    },
+    extend(config, ctx) {
+    }
   }
+
+
 }
