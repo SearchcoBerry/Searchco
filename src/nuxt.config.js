b5ffe630b347ab912ce5ccab089fc329c4391e5b
Kei Yoneda <keiyoneda@keinombp.lan>
2021-08-09 19:47:57 +0900

    fix build settings

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -1,6 +1,5 @@
 export default {
-  mode: 'universal',
-	target: 'static',
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Search Co. (さちこ)',
@@ -51,15 +50,14 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-
-  },
-
-  generate: {
-    async routes () {
-      const { $content } = require('@nuxt/content')
-      const files = await $content('news' || 'index').fetch()
-
-      return files.map(file => file.path === '/index' ? '/' : file.path)
+    /*
+    ** You can extend webpack config here
+    */
+    filenames: {
+      app: ({ isDev }) => isDev ? '[name].[hash].js' : '[chunkhash].js',
+      chunk: ({ isDev }) => isDev ? '[name].[hash].js' : '[chunkhash].js'
+    },
+    extend(config, ctx) {
     }
   }
 
